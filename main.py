a6dd48e169bdd214152454214c1cb8e3a33b2696
carlosq-mv <cquiroz7869@gmail.com>
2025-09-14 00:39:18 -0700

    chore: updates to other files

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 
 from router.server import server_api
+from router.test_api import test
+from db import engine_test, Base
+
+Base.metadata.create_all(bind=engine_test)
 
 # load any .env variables
 load_dotenv()
@@ -11,3 +15,4 @@ app = FastAPI()
 
 # include routers
 app.include_router(server_api, prefix="/server")
+app.include_router(test, prefix="/test")
