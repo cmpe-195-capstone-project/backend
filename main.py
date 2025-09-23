4eb73fabb146c4e2617a37590ed2eb75102c7633
carlosq-mv <cquiroz7869@gmail.com>
2025-09-22 17:20:57 -0700

    idk

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 from router.server import server_api
 from router.test_api import test
+from router.websocket import ws
 from db import engine_test, Base
 
 Base.metadata.create_all(bind=engine_test)
@@ -16,3 +17,4 @@ app = FastAPI()
 # include routers
 app.include_router(server_api, prefix="/server")
 app.include_router(test, prefix="/test")
+app.include_router(ws, prefix="/ws")
