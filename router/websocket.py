d4d3cf626ac21c0becb43a023def271edaeb2b52
carlosq-mv <cquiroz7869@gmail.com>
2025-09-28 21:57:29 -0700

    notification sys init

--- router/websocket.py
+++ router/websocket.py
@@ -1,75 +1,75 @@
 from fastapi import WebSocket, APIRouter, WebSocketDisconnect, Depends
 from sqlalchemy.orm import Session
-from db import get_db, FireModel
-from geopy.distance import geodesic
+from datetime import timedelta
+
+from db import get_db, get_test_db, FireModel
+from schema.fireschema import FireSchema
+from schema.userlocation import UserLocation
+from utils.geo import get_coordinates
+from utils.ws_manager import ConnectionManager
 
 ws = APIRouter()
 
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections = []
-    
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
-
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
-    
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
 manager = ConnectionManager()
+cache: dict[str, FireSchema] = {}
+DEFAULT_RADIUS = 10.0
 
-
-@ws.websocket("/notifcation")
-async def notification_endpoint(websocket: WebSocket, db: Session = Depends(get_db)):
+@ws.websocket("/alert")
+async def alert(websocket: WebSocket, db: Session = Depends(get_test_db)):
     await manager.connect(websocket)
     try:
         while True:
-            # long. and latt. of San Jose: User sends these points
+            # long. and latt. of San Jose: 
+            # User sends these points (example)
             # latt: 	37.335480
             # long:     -121.893028
             # radius:   10
             
             # expect data to be JSON with lattitude and longitude data fields
             data = await websocket.receive_json()
+            loc = UserLocation(**data)
 
-            # check the data (i.e. users location) to see if there are fires nearby
-            user_lat = data['lattitude']
-            user_long = data['longitude']
-            radius = data['radius']
 
-            # 1: look at active fires in db 
-            # active_fires = db.query(FireModel).filter(FireModel.final == False, FireModel.is_active == True).all()
-            # fire_notifications = []
-            # for fire in active_fires:
-            #     user_location = (user_lat, user_long)
-            #     fire_location = (fire.latitude, fire.longitude)
+            # create bouding box around users location 
+            bounding_box = get_coordinates(latitude=loc.latitude, longitude=loc.longitude, radius=loc.radius)
 
-            #     distance = geodesic(user_location, fire_location)
+            # select all fires in the box
+            fires = db.query(FireModel).filter(
+                FireModel.is_active == True,                 
+                FireModel.longitude.between(bounding_box.min_lon, bounding_box.max_lon),
+                FireModel.latitude.between(bounding_box.min_lat, bounding_box.max_lat)
+            ).all()
 
-            #     if distance < float(radius):
-            #         fire_notifications.append(fire)
-            
-            # 2: create a box around users location and query database for fires within that box
-            # need to get north point, south point, west point, east point all at a distance of 'radius'
-           
-             
-            
-            await manager.send_personal_message(f"list of all fires within {radius} of user's location", websocket)   
+            # list of alerts (in schema format)
+            fires_schemas = []
+            for fire in fires:
+                cache_key = fire.id
+                
+                # fire is not cached, means we can send alert 
+                if cache_key not in cache:
+                    fires_schemas.append(fire)
+                    cache[cache_key] = fire
 
-            san_jose = (user_lat, user_long) 
-            santa_clara=(37.354107, -121.955238)
-            distance = geodesic(san_jose, santa_clara).miles 
-             
-            if distance < float(radius): 
-                print("im sending notification")
+                # if it is in the cache, check if the fire has been updated
+                else:
+                    # check if it has been updated
+                    time_delta = fire.updated_datetime - cache[cache_key].updated_datetime
+                    if time_delta > timedelta(seconds=0) and cache[cache_key].is_active:
+                        # add the new fire to alert list and cache
+                        fires_schemas.append(fire)
+                        cache[cache_key] = fire
+        
+            if fires_schemas:
+                fires_schemas = [FireSchema.model_validate(fire, from_attributes=True) for fire in fires_schemas]
+                
+            # send the fires
+            if fires:
+                await manager.send_json_of_fires(fires=fires_schemas, websocket=websocket)
+            else:
+                await manager.send_json_message(message="No fires detected.", websocket=websocket)
 
-            
-            await manager.send_personal_message(f"distance is: {distance}", websocket)
     except WebSocketDisconnect:
+        await manager.send_message("Bye", websocket)
         manager.disconnect(websocket)
-        await manager.send_personal_message("Bye", websocket)
-    
-
+    except (ValueError, TypeError):
+        await manager.send_json_message({"error": "Invalid location data format"}, websocket)
